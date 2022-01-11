f830d10825116981bd8999394e91eb68ea57b548
Scott <88298032+scottdev1986@users.noreply.github.com>
2022-01-11 17:00:46 -0500

    adding changes to settings

--- portweb/settings.py
+++ portweb/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['159.223.189.187', 'localhost']
 
 
 # Application definition
