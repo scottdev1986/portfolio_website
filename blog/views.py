c8fa0d6f2d70545654deae342650bacb64165ca0
Scott <88298032+scottdev1986@users.noreply.github.com>
2022-01-12 14:18:07 -0500

    fixed blog views added filter to recent posts and sorting comments by date added

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,8 @@ from .models import Post, Comment
 
 def blog(request, slug):
     posts = get_object_or_404(Post,slug=slug)
-    recent_posts = Post.objects.all().order_by('-published_date')[:4:1]
-    comments = Comment.objects.all().filter(post=posts.id)
+    recent_posts = Post.objects.all().order_by('-published_date').filter(is_published=True)[:4:1]
+    comments = Comment.objects.all().order_by('-date_added').filter(post=posts.id)
 
     context = {
         'posts' : posts,
