0b82c0fca045fb752f79e0729b7d8105ea65ebbd
Scott <88298032+scottdev1986@users.noreply.github.com>
2022-01-12 01:10:09 -0500

    add pagination

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,7 @@
 from django import forms
+from django.core import paginator
 from django.shortcuts import render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from blog.models import Post
 from projects.models import Project
 from contacts.forms import ContactForm
@@ -8,9 +10,12 @@ def index(request):
     posts = Post.objects.order_by('-published_date').filter(is_published=True)
     projects = Project.objects.order_by('-published_date').filter(is_published=True)
     form = ContactForm
+    paginator = Paginator(posts, 3)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
 
     context = {
-        'posts' : posts,
+        'posts' : paged_listings,
         'projects' : projects,
         'form' : form
     }
